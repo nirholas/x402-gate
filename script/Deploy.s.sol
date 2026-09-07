7ca7b6b0a4997bb12829f2faf07429a86aa82eb1
nirholas <claudescammer@outlook.com>
2026-09-07 20:13:52 +0000

    chore: regenerate from the HookForge catalogue

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -36,6 +36,7 @@ import {X402GateHook} from "src/hooks/X402GateHook.sol";
 contract DeployX402Gate is Script {
     uint160 internal constant FLAGS = uint160(Hooks.AFTER_INITIALIZE_FLAG | Hooks.BEFORE_SWAP_FLAG);
 
+
     function run() external {
         IPoolManager manager = Chains.poolManager(block.chainid);
         require(address(manager) != address(0), "no Uniswap v4 PoolManager known for this chain");
